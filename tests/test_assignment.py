a051efcfcad9e9fd9f637c45fd2546fe01f40ccd
Ceay44 <cwilliams@perseverenow.org>
2025-04-12 16:52:24 -0500

    removed

--- tests/test_assignment.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from assignment import add_numbers  # Importing the student's function
-
-def test_addition():
-    assert add_numbers(2, 2) == 4
-    assert add_numbers(-1, 1) == 0
-    assert add_numbers(10, 5) == 15
