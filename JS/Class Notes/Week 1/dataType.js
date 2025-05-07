407e316dcf17559a81088ae05ac27cb1febfc396
Ceay44 <cwilliams@perseverenow.org>
2025-05-07 17:26:56 -0500

    update dt2

--- JS/Class Notes/Week 1/dataType.js
+++ JS/Class Notes/Week 1/dataType.js
@@ -47,30 +47,30 @@ console.log() // Exponentiation 3 ** 2 == 3 * 3
    // Multiple External scripts
 
 
-/** In JavaScript and also other programming languages, there are different types of data types. The following are JavaScript primitive data types: String, Number, Boolean, undefined, Null, and Symbol.
+// In JavaScript and also other programming languages, there are different types of data types. The following are JavaScript primitive data types: String, Number, Boolean, undefined, Null, and Symbol.
 
-/// Numbers
-Integers: Integer (negative, zero and positive) numbers Example:
- ... -3, -2, -1, 0, 1, 2, 3 ...
+// /// Numbers ///
+// Integers: Integer (negative, zero and positive) numbers Example:
+//  ... -3, -2, -1, 0, 1, 2, 3 ...
 
-Float-point numbers: Decimal number Example:
- ... -3.5, -2.25, -1.0, 0.0, 1.1, 2.2, 3.5 ...
+// Float-point numbers: Decimal number Example:
+//  ... -3.5, -2.25, -1.0, 0.0, 1.1, 2.2, 3.5 ...
 
-/// Strings
-A collection of one or more characters between two single quotes, double quotes, or backticks. 
+// /// Strings ///
+// A collection of one or more characters between two single quotes, double quotes, or backticks. 
 
-/// Booleans
-A boolean value is either True or False. Any comparisons returns a boolean value, which is either true or false.
+// /// Booleans ///
+// A boolean value is either True or False. Any comparisons returns a boolean value, which is either true or false.
 
-A boolean data type is either a true or false value.
+// A boolean data type is either a true or false value.
 
-/// Undefined
-In JavaScript, if we don't assign a value to a variable, the value is undefined. In addition to that, if a function is not returning anything, it returns undefined.
+// /// Undefined ///
+// In JavaScript, if we don't assign a value to a variable, the value is undefined. In addition to that, if a function is not returning anything, it returns undefined.
 
-/// Null
-Null in JavaScript means an empty value.
+// /// Null /////
+// Null in JavaScript means an empty value.
 
-*/
+
 
 
 
